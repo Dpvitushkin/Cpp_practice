3432c5e47e4de57365afa4a495e6c99a5ca7acfa
Dpvitushkin <74239991+Dpvitushkin@users.noreply.github.com>
2022-08-18 22:11:51 +0300

    18.08-22:11

--- begginer/1.cpp
+++ begginer/1.cpp
@@ -1,8 +1,25 @@
+//Составить программу, которая будет считывать введённое пятизначное число. 
+//После чего, каждую цифру этого числа необходимо вывести в новой строке.
+
+
 #include <iostream>
+int digit(int x,int num)
+{
+    int result;
+    for (int i=0;i<num;i++)
+    {
+        result=x%10;
+        x=x/10;
+    }
+    return result;
+}
 
 int main()
 {
-    std::cout<<"Hello"<<std::endl;
-    std::cout<<"123";
-    
+    int x;
+    std::cin>>x;
+    for(int i=5;i>0;i--)
+    {
+        std::cout<<digit(x,i)<<std::endl;
+    }
 }

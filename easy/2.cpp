cc66dd6b31af60a39241e17cc3cf995b50740c7f
Dpvitushkin <74239991+Dpvitushkin@users.noreply.github.com>
2022-08-19 14:18:39 +0300

    19.08-14:17

--- easy/2.cpp
+++ easy/2.cpp
@@ -1,6 +1,16 @@
+//Найти алгебраическую сумму для выражения: 1k + 2k + 3k + … + Nk. N и степень k вводит пользователь.
 #include <iostream>
+#include <cmath>
 
 int main()
 {
-    std::cout<<"Privet";
+    int n,k;
+    std::cout<<"Enter n and k:"<<std::endl<<"n=";
+    std::cin>>n;
+    std::cout<<"k=";
+    std::cin>>k;
+
+    int result=0;
+    for(int i=1;i<=n;i++) {result+=pow(i,k);}
+    std::cout<<result<<std::endl;
 }

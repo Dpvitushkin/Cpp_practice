6b5e8e1707a84085867d0036fe79cdee4cc24161
Dpvitushkin <74239991+Dpvitushkin@users.noreply.github.com>
2022-08-26 14:40:58 +0300

    26.08-14:40

--- tests/1.cpp
+++ tests/1.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
-#include <string>
+#include <vector>
+
+struct elem{
+    int val;
+    int level;
+};
 
 int main()
 {
-    std::string str="abcde";
-    std::cout<<str.at(1);
-    std::cout<<str[1];
-
+    std::vector<elem> A;
+    elem x;
+    x.level=1;
+    x.val=2;
+    A.push_back(x);
+    std::cout<<"print: "<<A[0].le
 }

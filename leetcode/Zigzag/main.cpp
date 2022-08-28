c9edaab4de9ee6530ac2ae9ce03bd592b7c20223
Dpvitushkin <74239991+Dpvitushkin@users.noreply.github.com>
2022-08-28 22:05:22 +0300

    28.08-22:05

--- leetcode/Zigzag/main.cpp
+++ leetcode/Zigzag/main.cpp
@@ -18,19 +18,81 @@ std::vector<elem> fill_structure(std::string str,int num)
     for(int i=0;i<str.size();i++)
     {
         c=str[i];
-        if(order=="down" && cur_level<=4)
+        x.val=c;
+        x.level=cur_level;
+        x.number=i;
+        A.push_back(x);
+        if(order=="down")
         {
-            x.val=c;
-            x.level=cur_level;
-            x.number=i;
-            A.push_back(x);
-            //закончил тут
-            
+            if(cur_level==num)
+            {
+                order="up";
+                cur_level--;
+                continue;
+            }
+            else
+            {
+                cur_level++;
+                continue;
+            }   
+        }
+        else
+        {
+            if(cur_level==1)
+            {
+                order="down";
+                cur_level++;
+                continue;
+            }
+            else
+            {
+                cur_level--;
+                continue;
+            }  
+
         }
 
-
     }
+    return A;
 
+
+}
+std::string make_str(std::vector<elem> A,int num)
+{
+    std::cout<<"started make_str"<<std::endl;
+    std::string tmp,result,str_out="",tmp_1,tmp_2;
+    int h_glob=2*num-2;
+    int h_cur=0;
+    for(int lev=1;lev<=num;lev++)
+    {
+        while(1)
+        {
+            try
+            {
+                if(lev==num || lev==1)
+                {
+                    tmp+=A.at(h_cur+lev-1).val;
+                    h_cur+=h_glob;
+                    continue;
+                }
+                tmp_1=A.at(h_cur+lev-1).val;
+                tmp+=tmp_1;
+                tmp_2=A.at(h_cur+h_glob-lev+1).val;
+                tmp+=tmp_2;
+                std::cout<<"    tmp="<<tmp<<"lev="<<lev<<std::endl;
+                h_cur+=h_glob;
+            }
+            catch(...)
+            {
+                std::cout<<"catched"<<std::endl;
+                h_cur=0;
+                result+=tmp;
+                tmp="";
+                break;
+            }
+        }
+    }
+    return result;
 
 }
 int main()
@@ -39,8 +101,16 @@ int main()
     int num;
     std::cout<<"enter string:"<<std::endl<<"   str= ";
     std::cin>>str;
-    std::cout<<"enter number of levels:"<<std::endl<<"   num=";
+    std::cout<<"size="<<str.size()<<std::endl<<"enter number of levels:"<<std::endl<<"   num=";
     std::cin>>num;
+    std::vector<elem> A=fill_structure(str,num),B;
+    for(int i=0;i<A.size();i++)
+    {
+        std::cout<<A[i].val<<" "<<A[i].level<<" "<<A[i].number<<std::endl;
+    }
+    std::string result=make_str(A,num);
+    std::cout<<result<<"  size="<<result.size()<<std::endl;
+    
 
 
 }
--- tests/1.cpp
+++ tests/1.cpp
@@ -8,10 +8,29 @@ struct elem{
 
 int main()
 {
-    std::vector<elem> A;
-    elem x;
-    x.level=1;
-    x.val=2;
-    A.push_back(x);
-    std::cout<<"print: "<<A[0].le
+    std::vector<int> a;
+    a.push_back(1);
+    a.push_back(2);
+    a.push_back(3);
+    a.push_back(4);
+    a.push_back(5);
+    std::cout<< "size= "<<a.size()<<std::endl;
+    int i=0;
+    while (1)
+    {
+        try
+        {
+            std::cout<<a.at(i);
+        
+        }
+        catch(...)
+        {
+            std::cout<<"catched"<<std::endl;
+            break;
+
+        }
+        i++;
+    }
+    std::cout<<"end";
+    
 }
